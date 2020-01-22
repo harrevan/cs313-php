601bc10625f5f9dec0502358f1fec175d1f0659d
Evan Harrison <har16143@byui.edu>
2020-01-21 18:14:53 -0700

    finish

--- web/teach02/basic.php
+++ web/teach02/basic.php
@@ -1,8 +1,10 @@
 <html>
 <body>
 
-Welcome <?php $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
+Welcome <?php echo $_POST["name"]; ?><br>
+Email: <?php echo $_POST["email"]; ?><br>
+Major: <?php echo $_POST["major"]; ?><br>
+Comments: <?php echo $_POST["comment"]; ?><br>
 
 </body>
 </html>
