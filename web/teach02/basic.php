5e9c8eb612832bd88bb4119748a5c3f40a504902
Evan Harrison <har16143@byui.edu>
2020-01-21 18:30:48 -0700

    finish

--- web/teach02/basic.php
+++ web/teach02/basic.php
@@ -5,7 +5,12 @@ Welcome <?php echo $_POST["name"]; ?><br>
 Email: <?php echo $_POST["email"]; ?><br>
 Major: <?php echo $_POST["major"]; ?><br>
 Comments: <?php echo $_POST["comment"]; ?><br>
-Continents: <?php echo $_POST["continent"]; ?><br>
+Continents:
+for($i=0; $i < 7; $i++)
+{
+	<?php echo $_POST["continent"][$i]; ?><br>
+}
+
 
 </body>
 </html>
