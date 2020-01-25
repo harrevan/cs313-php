520f3143784c9a35ec98751a4229c604ba8d3686
Evan Harrison <har16143@byui.edu>
2020-01-25 09:45:28 -0700

    browse

---
+++ web/teach03/checkout.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="disc.css">
+    <title>Cart</title>
+  </head>  
+  <body>
+    <div class="jumbotron text-center">
+      <div class="container">
+      <h1>Disc Golf Distributor</h1>
+      <p>Your most trusted disc supplier serving Southeastern Idaho.</p>
+      </div>
+    </div>	
+    <a href="browse.php">Back to browse</a>
+    <a href="confirmation.php">Checkout</a>
+    <div class="centerAll">
+      <h2 class="heading">Checkout</h2>	
+      <hr>
+    </div>	
+  </body>
+</html>
