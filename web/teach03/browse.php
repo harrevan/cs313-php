5c54ce70e6350ba04a043e5dbf6ba2dbcc26a400
Evan Harrison <har16143@byui.edu>
2020-01-25 09:58:47 -0700

    browse

--- web/teach03/browse.php
+++ web/teach03/browse.php
@@ -41,16 +41,16 @@
          $star = "$17.00";
          $boss = "Boss- Champion plastic ". "Price: " . "<b>$champ</b>";
          $destroyer = "Destroyer- Champion plastic " . "Price: " .  "<b>$champ</b>";
-         $beast = "Beast- DX plastic ". "$<b>dx</b>";
+         $beast = "Beast- DX plastic ". "<b>$dx</b>";
          $road_runner = "Road Runner- Star plastic " . "Price: " .  "<b>$star</b>";
          $wraith = "Wraith- Star plastic " . "<b>$star</b>";
          $banshee = "Banshee- Champion plastic " . "Price: " . "<b>$champ</b>";
-         $eagle = "Eagle- DX plastic " . "Price: " . "$<b>dx</b>";
-         $leopard = "Leopard- DX plastic " . "Price: " . "$<b>dx</b>";
+         $eagle = "Eagle- DX plastic " . "Price: " . "<b>$dx</b>";
+         $leopard = "Leopard- DX plastic " . "Price: " . "<b>$dx</b>";
          $teebird = "Teebird- Star plastic " . "Price: " .  "<b>$star</b>";
-         $aviar = "Aviar- DX plastic " . "Price: " . "$<b>dx</b>";
-         $dart = "Dart- DX plastic " . "Price: " . "$<b>dx</b>";
-         $pig = "Pig- DX plastic " . "Price: " . "$<b>dx</b>";
+         $aviar = "Aviar- DX plastic " . "Price: " . "<b>$dx</b>";
+         $dart = "Dart- DX plastic " . "Price: " . "<b>$dx</b>";
+         $pig = "Pig- DX plastic " . "Price: " . "<b>$dx</b>";
          $slammer = "Slammer- Champion plastic " . "Price: " . "<b>$champ</b>";
 
          
--- web/teach03/checkout.php
+++ web/teach03/checkout.php
@@ -23,6 +23,42 @@ session_start();
     <div class="centerAll">
       <h2 class="heading">Checkout</h2>	
       <hr>
-    </div>	
+    </div>
+      <div class="inline">
+        <label>First Name</label>
+        <br>
+        <input type="text" id="name" name="firstName" placeholder="William">
+        <span class="error" id="fname">Please enter your first name</span>
+        <br>
+        <label>Last Name</label>
+        <br>
+        <input type="text" id="name" name="lastName" placeholder="Wallace">
+        <span class="error" id="lname">Please enter your last name</span>
+        <br>
+         <label>Phone Number</label>
+        <br>
+        <input type="text" id="phone" placeholder="123-123-1234" name="phoneNumber">
+        <span class="error" id="ph">Please enter your phone number</span>
+        <br>
+        <label>Billing Address</label>
+        <br>
+        <input type="text" id="adress" name="address" placeholder="115 John Adams Street">
+        <span class="error" id="addy">Please enter your street address</span>
+        <br>
+        <label>City</label>
+        <br>
+        <input type="text" placeholder="Idaho Falls" name="cityName">
+        <span class="error" id="city">Please enter your city</span>
+        <br>
+        <label>State</label>
+        <br>
+        <input type="text" id="state" placeholder="ID" name="stateName">
+        <span class="error" id="st">Please enter your state abbreviation in the format: AL</span>
+        <br>
+        <label>Zip Code</label>
+        <br>
+        <input type="text" id="zipc" placeholder="83401" name="zipCode">
+        <span class="error" id="zip">Please enter your 5 digit zip code</span>
+      </div>    	
   </body>
 </html>
