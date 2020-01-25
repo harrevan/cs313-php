dbbd4429dc0cebad629ea465f8f27ce41d126ede
Evan Harrison <har16143@byui.edu>
2020-01-24 17:32:11 -0700

    browse

--- web/teach03/browse.php
+++ web/teach03/browse.php
@@ -8,7 +8,7 @@
     <!--Include Bootstrap compiled CSS. Most of the CSS in this page consists of built-in Bootstrap classes. -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
-    <link rel="stylesheet" type="text/css" href="homestyles.css">
+    <link rel="stylesheet" type="text/css" href="disc.css">
     
     <!--Include Bootstrap compiled JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
@@ -30,7 +30,7 @@
     <br>
     <div class="row">
       <div class="col">
-         <img class src="boss.jpg">
+         <img class="discpic" src="boss.jpg">
          <br>
          Boss
       </div>
