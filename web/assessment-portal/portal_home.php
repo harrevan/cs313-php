3427d228c457cf2005e7392822b38d3d45733ebe
Evan Harrison <har16143@byui.edu>
2020-02-01 12:49:25 -0700

    home

--- web/assessment-portal/portal_home.php
+++ web/assessment-portal/portal_home.php
@@ -13,6 +13,9 @@
     </script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
     </script> 
+
+    <link rel="stylesheet" type="text/css" href="assessments.css">
+
     <title>Assessment Portal</title>
   </head>
   <?php
@@ -50,7 +53,7 @@
       </ul>
 
       <!-- The slideshow -->
-      <div class="carousel-inner">
+      <div class="carousel-inner div_back_color">
         <div class="carousel-item active">
           <img src="kids.png">
         </div>
