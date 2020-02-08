7ce08ad7b178f144f958351202bc099735d4ad7d
Evan Harrison <har16143@byui.edu>
2020-02-07 18:07:05 -0700

    database connection test

---
+++ web/assessment-portal/class_am.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <!--Include Bootstrap compiled CSS. Most of the CSS in this page consists of built-in Bootstrap classes. -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">  
+    <!--Include Bootstrap compiled JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
+    </script> 
+
+    <link rel="stylesheet" type="text/css" href="assessment.css">
+
+    <title>Class-AM</title>
+  </head>
+  <?php
+      session_start();
+  ?>
+  <body>
+    <?php
+      try
+      {
+        $dbUrl = getenv('DATABASE_URL');
+
+        $dbOpts = parse_url($dbUrl);
+
+        $dbHost = $dbOpts["host"];
+        $dbPort = $dbOpts["port"];
+        $dbUser = $dbOpts["user"];
+        $dbPassword = $dbOpts["pass"];
+        $dbName = ltrim($dbOpts["path"],'/');
+
+        $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      }
+      catch (PDOException $ex)
+      {
+        echo 'Error!: ' . $ex->getMessage();
+        die();
+      }
+    ?>  
+    <?php
+      foreach ($db->query('SELECT student_name, student_id FROM students') as $row)
+      {
+        echo 'student: ' . $row['student_name'];
+        echo ' id: ' . $row['student_id'];
+        echo '<br/>';
+      }
+    ?>
+
+   <!-- <div class="row">
+      <div class="col">
+         <img class="discpic" src="boss.jpg">
+         <p class="pdiscs">Boss- Champion plastic<br><b>$15.00</b></p>
+         <form method="post">
+           <button type="submit" id="add" class="btn btn-info" name="boss_disc">Add To Cart</button>
+         </form>
+      </div>
+      <div class="col">
+        <img class="discpic" src="destroyer.jpg">
+        <p class="pdiscs">Destroyer- Champion plastic<br><b>$15.00</b></p>
+        <form method="post">
+          <button type="submit" id="add" class="btn btn-info" name="destroyer_disc">Add To Cart</button>
+        </form>
+      </div>
+    </div>
+    <br>
+      </div>
+    </div>    -->  
+  </body>
+</html>
--- web/assessment-portal/portal_home.php
+++ web/assessment-portal/portal_home.php
@@ -29,7 +29,7 @@
             <a class="nav-link" href="#">Portal Home</a>
           </li>
           <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <a class="nav-link dropdown-toggle" href="class_am.php" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               Class- AM
             </a>
             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
