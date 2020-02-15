014aa5fef17eddc03dd0986b42894d93dc61cd29
Evan Harrison <har16143@byui.edu>
2020-02-15 09:35:40 -0700

    database connection test

--- web/assessment-portal/class_am.php
+++ web/assessment-portal/class_am.php
@@ -146,7 +146,7 @@
           <h2 id="centerform"><?php echo "Unit " . $_POST['assessments'] . " " . $_POST['subject'] . " ";?> Assessment Scores</h2>  
             <?php
               for($i = 0; $i < sizeof($assessments); $i++){
-                echo $assessments[$i]['assesment_title'] . "<br>";
+                echo sizeof($assessments);
 
               }
 
