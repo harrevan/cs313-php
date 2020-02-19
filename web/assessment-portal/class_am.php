542953343771aa27e521eecd86de9bf2cd668a47
Evan Harrison <har16143@byui.edu>
2020-02-18 18:26:48 -0700

    database connection test

--- web/assessment-portal/class_am.php
+++ web/assessment-portal/class_am.php
@@ -65,14 +65,15 @@
         echo 'Error!: ' . $ex->getMessage();
         die();
       }
-        $assessments = "SELECT assessment_title FROM master_assessment  WHERE class_time = '{$_POST["time"]}' AND subject = '{$_POST["subject"]}' AND assessment_period = '{$_POST["assessments"]}'";
-        $stmt = $db->prepare($assessments);
-        $stmt->execute();
-        $assessments = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
         // Find total # of students by class and assessment with MT score
         if(isset($_POST['assessments']))
         {
+        $assessments = "SELECT assessment_title FROM master_assessment  WHERE class_time = '{$_POST["time"]}' AND subject = '{$_POST["subject"]}' AND assessment_period = '{$_POST["assessments"]}'";
+        $stmt = $db->prepare($assessments);
+        $stmt->execute();
+        $assessments = $stmt->fetchAll(PDO::FETCH_ASSOC);
+          
         $scores = "SELECT count(score), assessment_title, score 
                             FROM assessment_score 
                             INNER JOIN students ON students.student_id = assessment_score.student_id 
