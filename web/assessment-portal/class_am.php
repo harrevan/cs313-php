a2bc164962cc6c56ac188d59e51c19e7e69409ad
Evan Harrison <har16143@byui.edu>
2020-02-18 18:49:51 -0700

    database connection test

--- web/assessment-portal/class_am.php
+++ web/assessment-portal/class_am.php
@@ -166,7 +166,7 @@
               </thead>
               <tbody>  
              <?php
-              for($i = 0; $i < sizeof($scores); $i++)
+              for($i = 0; $i < sizeof($assessments); $i++)
               {
               ?>   
                 <tr>
