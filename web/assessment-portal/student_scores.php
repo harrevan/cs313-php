baaa7710ca169b3f3c75c317f83b43cb711cae06
Evan Harrison <har16143@byui.edu>
2020-02-21 17:44:07 -0700

    final revisons

--- web/assessment-portal/student_scores.php
+++ web/assessment-portal/student_scores.php
@@ -60,8 +60,8 @@
       
       if(isset($_SESSION['assessments']) && !empty($_SESSION['assessments']) && isset($_SESSION['subject']) && !empty($_SESSION['subject']))
       {
-        $select_assessments = $db->query("SELECT assessment_id, assessment_title FROM master_assessment WHERE assessment_period = '{$_SESSION["assessments"]}' AND subject = '{$_SESSION["subject"]}'");
-        $assessment_rows = $select_assessments->fetchAll(PDO::FETCH_ASSOC);
+        //$select_assessments = $db->query("SELECT assessment_id, assessment_title FROM master_assessment WHERE assessment_period = '{$_SESSION["assessments"]}' AND subject = '{$_SESSION["subject"]}'");
+        //$assessment_rows = $select_assessments->fetchAll(PDO::FETCH_ASSOC);
       }
 
 ?> 
