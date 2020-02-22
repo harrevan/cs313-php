50ee9873dcea38ea7bfec57b420e96e0f58a3b32
Evan Harrison <har16143@byui.edu>
2020-02-22 14:31:55 -0700

    final revisons

--- web/assessment-portal/class_am.php
+++ web/assessment-portal/class_am.php
@@ -196,7 +196,7 @@
                   <td><?php echo $assessments[$i]['assessment_title']; ?></td>
                   <td>
                     <?php
-                      if($mt_scores[$i]['count'])
+                      if($mt_scores[$i]['assessment_title'] = $assessments[$i]['assessment_title'])
                       {
                       if($_POST['time'] == 'AM')
                       {
@@ -216,7 +216,7 @@
                   </td>
                   <td>
                     <?php
-                      if($nt_scores[$i]['count'])
+                      if($nt_scores[$i]['assessment_title'] = $assessments[$i]['assessment_title'])
                       {
                         if($_POST['time'] == 'AM')
                         {
@@ -235,7 +235,7 @@
                   </td>
                   <td>
                     <?php
-                      if($bt_scores[$i]['count'])
+                      if($bt_scores[$i]['assessment_title'] = $assessments[$i]['assessment_title'])
                       {
                         if($_POST['time'] == 'AM')
                         {
