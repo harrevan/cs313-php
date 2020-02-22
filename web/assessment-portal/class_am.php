2d34a4274c6dfe4eb3f61e226697689b7b76c79e
Evan Harrison <har16143@byui.edu>
2020-02-22 10:55:03 -0700

    final revisons

--- web/assessment-portal/class_am.php
+++ web/assessment-portal/class_am.php
@@ -14,6 +14,10 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
     </script> 
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type = "text/javascript">
+      google.charts.load("current", {"packages":["corechart"]});
+      google.charts.setOnLoadCallback(drawChart);
+    </script>           
 
     <link rel="stylesheet" type="text/css" href="assessment.css">
 
@@ -139,8 +143,7 @@
                
 
                 // Load google charts
-                google.charts.load("current", {"packages":["corechart"]});
-                google.charts.setOnLoadCallback(drawChart);
+
 
                 // Draw the chart and set the chart values
                 function drawChart() {
